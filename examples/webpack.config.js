e2dced2fcde2a098c858780bfc6b38ebaa0fad94
Pellegrino Prevete <dvorak@0x87003Bd6C074C713783df04f36517451fF34CBEf>
2025-11-08 17:04:17 +0000

    hurr

---
+++ examples/webpack.config.js
@@ -0,0 +1,27 @@
+let
+  _file_name,
+  _output,
+  _output_dir,
+  _path;
+_path =
+  require(
+    'path');
+_output_dir =
+  _path.resolve(
+    __dirname);
+_file_name =
+  "ahsi.js";
+
+_output = {
+  path:
+    _output_dir,
+  filename:
+    _file_name
+}
+
+module.exports = {
+  entry:
+    './ahsi',
+  output:
+    _output
+};
